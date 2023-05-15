3f9c510606352a3844acac7d512e0af5902f91de
sabina-045 <116088527+sabina-045@users.noreply.github.com>
2023-05-15 10:42:23 +0700

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.19.39', ]
+ALLOWED_HOSTS = [os.getenv('PUBLIC_IP', default='127.0.0.1')]
 
 
 INSTALLED_APPS = [
