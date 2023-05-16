1a0ddef18645c23d8ea1fc96a9f1b514c2a88623
sabina-045 <116088527+sabina-045@users.noreply.github.com>
2023-05-16 22:59:16 +0700

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,8 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = False
+USE_X_FORWARDED_HOST = True
 
-ALLOWED_HOSTS = ['*', '158.160.19.39', 'localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = ['158.160.19.39', 'localhost', '127.0.0.1', ]
 
 
 INSTALLED_APPS = [
