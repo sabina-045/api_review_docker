94bb2fbb87749acfde67026b49d5d4d42277a172
sabina-045 <116088527+sabina-045@users.noreply.github.com>
2023-05-17 12:22:22 +0700

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = False
-USE_X_FORWARDED_PORT = True
+
 ALLOWED_HOSTS = ['158.160.19.39', 'localhost', '127.0.0.1', ]
 
 
